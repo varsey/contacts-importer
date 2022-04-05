97dc2d1364778e69c15f17abefdb36d8fc889b1a
varsey <eugeny.varseev@gmail.com>
2022-04-06 00:05:32 +0300

    feat [importer] registration template updated

--- importer/views.py
+++ importer/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def registeruser(request):
-    return render(request, 'importer/registeruser.html')
+
+    return render(request, 'importer/registeruser.html', {'form':UserCreationForm()})
